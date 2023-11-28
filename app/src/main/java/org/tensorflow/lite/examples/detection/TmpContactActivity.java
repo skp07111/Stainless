2cb380afa014029f45385509fa426c57984fcbd5
seoyoung <si159521@gmail.com>
2023-11-29 01:58:03 +0900

    설정/연락처 이미지

---
+++ app/src/main/java/org/tensorflow/lite/examples/detection/TmpContactActivity.java
@@ -0,0 +1,13 @@
+package org.tensorflow.lite.examples.detection;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TmpContactActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.temporary_contactlist);
+    }
+}
